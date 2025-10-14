d90b13352dae21118eab9585e7a27499c382e68b
Churchill <reghoyah@gmail.com>
2025-10-14 02:00:03 +0000

    set up the js file with data fetching

---
+++ nexus-project/src/services/jikanApi.js
@@ -0,0 +1,30 @@
+const BASE_URL = 'https://api.jikan.moe/v4'; //wrapped the api base url in a constant
+
+export const searchAnime = async (query) => {
+    const response = await fetch(`${BASE_URL}/anime?q=${query}&limit=5`); 
+    if (!response.ok) {
+        throw new Error(`HTTP ${response.status}: ${response.statusText}`) //error handling the response the api gives
+    }
+    const data = await response.json();
+    return data; //returning the data
+};
+
+export const getAnimeById = async (id) => {
+    const detailsResponse = await fetch(`${BASE_URL}/anime/${id}/full`) //get full details of an anime by its id
+    if(!detailsResponse.ok) {
+        throw new Error(`HTTP ${detailsResponse.status}: ${detailsResponse.statusText}`) //error handling the response the api gives
+    }
+    const detailsData = await detailsResponse.json();
+    return detailsData; //returning the data
+}
+
+export const getAnimebygenre = async (genreID) => {
+    const genreResponse = await fetch(`${BASE_URL}/anime?genres=${genreID}&limit=10&order_by=score&sort=desc&limit=`) //get anime by genre id
+    if (!genreResponse.ok) {
+        throw new Error(`HTTP ${genreResponse.status}: ${genreResponse.statusText}`) //error handling the response the api gives
+    }
+    dataResponse = await genreResponse.json();
+    return dataResponse; //returning the data
+}
+export default BASE_URL;
+
