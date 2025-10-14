50eb8a8c70419ab93a662d6ac17d72f3caf3c81f
Churchill <reghoyah@gmail.com>
2025-10-14 09:57:22 +0000

    setup zustand store with inital state

---
+++ nexus-project/src/store/store.js
@@ -0,0 +1,42 @@
+import { create } from 'zustand';
+
+const useAnimeStore = create((set) => ({
+    //state for search functionality
+    searchQuery: '',
+    searchResults: [],
+    searchError: null,
+    searchLoading: false,
+
+    //state for browse by genre
+    genreData: {
+        action: [],
+        comedy: [],
+        drama: [],
+        romance: [],
+        fantasy: []
+    },
+    genreLoading: false,
+    genreError: null,
+
+    //state for anime details
+    selectedAnime: null,
+    selectedAnimeLoading: false,
+    selectedAnimeError: null,
+
+    //actions for search functionality
+    setSearchQuery: (query) => set({searchQuery: query}),
+    setSearchResults: (results) => set({searchResults: results}),
+    setSearchError: (error) => set({searchError: error}),
+    setSearchLoading: (isLoading) => set({searchLoading: isLoading}),
+
+    //actions for browse by genre
+
+
+    //actions for anime details
+    setSelectedAnime: (anime) => set({selectedAnime: anime}),
+    setSelectedAnimeLoading: (isloading) => set({selectedAnimeLoading: isloading}),
+    setSelectedAnimeError: (error) => set({selectedAnimeError: error})
+}))
+
+
+export default useAnimeStore;
