bba6da16b63e71c45c00820ccd94a6d6e5896024
Churchill <reghoyah@gmail.com>
2025-10-18 12:41:00 +0000

    spent 2 days trying to get the correct genre id from jikan to make it get alll anime. currently exploring other options but initial set up here

--- nexus-project/src/pages/Browsepage.jsx
+++ nexus-project/src/pages/Browsepage.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import Navigation from "../components/Navigation";
+import Animecard from "../components/Animecard";
+import { Link } from "react-router-dom";
+import { getAnimeByGenre } from "../services/jikanApi";
+import useAnimeStore from "../store/store";
+
 function Browsepage() {
-    return(
-        <div>
-            <h1>Browsepage</h1>
-        </div>
-    );
+    const{genreData, genreloading, setGenreData, setGenreLoading} = useAnimeStore() //store valuse from the store js file and setvalues as well
+
+    useEffect(() => {
+        const fetchAllGenres = async () => {
+            setGenreLoading(true)
+
+            try{
+                for (const[genreName, genreId] of Object.entries(GENRES)) {
+                    const response = await getAnimeByGenre(genreId)
+                    const animeArray = response.data
+                    setGenreData(genreName, animeArray)
+                }
+            }
+            catch(error) {
+                console.log(error)
+            }
+            finally {
+                setGenreLoading(false)
+            }
+        }
+        fetchAllGenres()
+    }, []) 
 }
 
 export default Browsepage;
