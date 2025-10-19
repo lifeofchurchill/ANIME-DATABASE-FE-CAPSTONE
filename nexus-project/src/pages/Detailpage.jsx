8bc2918ce2e7125ccd3e718349b8a83beae90bc9
Churchill <reghoyah@gmail.com>
2025-10-19 08:56:04 +0000

    made some corrections in the api handling by removing the unnecesary .data() and also added a startup detail page with image handling title synopsis etc

--- nexus-project/src/pages/Detailpage.jsx
+++ nexus-project/src/pages/Detailpage.jsx
@@ -8,8 +8,7 @@ function Detailpage () {
     const { id } = useParams();  //gets id from url for example /anime/5 hence yrl is 5
 
     const {
-        selectedAnime, detailLoading, 
-        detailError, setSelectedAnime, 
+        selectedAnime, setSelectedAnime, 
         setSelectedAnimeLoading, setSelectedAnimeError
     } = useAnimeStore() //store values from zustand
 
@@ -19,10 +18,7 @@ function Detailpage () {
 
             try {
                 const response = await getAnimeById(id) //getting anime by id from the API
-
-                const animeData = response.data() //extracting the data from the response the API gives me
-
-                setSelectedAnime(animeData) //storing it in the store
+                setSelectedAnime(response.data) //storing it in the store
             }
             catch(error) {
                 setSelectedAnimeError(error.message) //handling errors incase theres an error 
@@ -33,5 +29,43 @@ function Detailpage () {
         }
         fetchAnimeDetails()
     }, [id]); // the array with id just means rerun if id changes
+
+    return (
+        <div className="min-h-screen">
+            <Navigation />
+            <div className="Container">
+                {selectedAnime ? (
+                    <>
+                        {selectedAnime.images?.jpg?.large_image_url && (
+                            <img 
+                                src={selectedAnime.images.jpg.large_image_url}
+                                alt={selectedAnime.title} 
+                                className="h-90 w-150 rounded-lg"
+                            />
+                        )}
+
+                        <h1 className="font-bold">{selectedAnime.title}</h1>
+
+                        <p>{selectedAnime.synopsis}</p>
+
+                        <p>
+                            <span className="font-semibold mr-2">Release year:</span>
+                            {selectedAnime.year || 'N/A'}
+                        </p>
+
+                        <p>
+                            <span className="font-semibold mr-2">Airing Status:</span>
+                            {selectedAnime.status}
+                        </p>
+                    </>
+                ) : (
+                    <p>Loading...</p>
+                )}
+            </div>
+
+        </div>
+    )
 }
+
+export default Detailpage
 
